056b7520233559d5de4157db02704862ec9d605f
Phil Seeley <phil.seeley@gmail.com>
2018-04-13 19:20:42 +1000

    Functionaly working.

---
+++ lib/Receipts.dart
@@ -0,0 +1,61 @@
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'dart:io';
+import 'dart:convert';
+
+class Reciept {
+  final DateTime date;
+  final String place;
+  final double value;
+  
+  Reciept(this.date, this.place, this.value);
+
+
+  Map toJson(){
+     return { 'date': date.toIso8601String(), 'place': place, 'value': value };
+  }
+}
+
+class Receipts {
+  final List<Reciept> current = new List<Reciept>();
+  final List<Reciept> checked = new List<Reciept>();
+  File _store;
+
+  Map toJson(){
+    return { 'current': current, 'checked': checked.toList() };
+  }
+
+  load() async {
+    print('loading');
+    Directory directory = await path_provider.getApplicationDocumentsDirectory();
+    _store = new File('${directory.path}/receipts.json');
+
+    try {
+      String s = _store.readAsStringSync();
+      dynamic data = json.decode(s);
+
+      for (var r in data['current'])
+        current.add(_parse(r));
+
+      for (var r in data['checked'])
+        checked.add(_parse(r));
+    } on FileSystemException {}
+  }
+
+  Reciept _parse(dynamic r){
+    DateTime d = DateTime.parse(r['date']);
+    String l = r['place'];
+    double v = r['value'];
+
+    return new Reciept(d, l, v);
+  }
+  
+  save (){
+    print('saving');
+/*
+    int i=0;
+
+    data['current'] = current[0];
+*/
+    _store.writeAsStringSync(json.encode(this));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,162 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:collection';
+import 'dart:async';
+import 'dart:io';
 import 'package:location/location.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'Receipts.dart';
 
 void main() => runApp(new ReceiptsApp());
-///
+
 class ReceiptsApp extends StatelessWidget {
+  static final Receipts receipts = new Receipts();
+
   @override
   Widget build(BuildContext context) {
+
     return new MaterialApp(
       title: 'Receipts',
-      home: new Receipts(),
+      home: new NewReceiptWidget(),
     );
   }
 }
 
-class Receipts extends StatefulWidget {
+class NewReceiptWidget extends StatefulWidget {
   @override
-  createState() => new ReceiptsState();
+  createState() => new NewReceiptState();
 }
 
-class Reciept {
-  DateTime date;
-  String location;
-  double value;
-  Reciept(DateTime d, String l, double v) {
-    date = d;
-    location = l;
-    value = v;
-  }
-}
-class ReceiptsState extends State<Receipts> {
-  final List<String> _places = <String>[];
-  final Queue<Reciept> _receipts = new Queue<Reciept>();
-  final Queue<Reciept> _doneReceipts = new Queue<Reciept>();
-  final TextStyle _biggerFont = const TextStyle(color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.bold);
-  TextField _valueTextField;
+class NewReceiptState extends State<NewReceiptWidget> with WidgetsBindingObserver {
+  Receipts _receipts;
+  StreamSubscription<Map<String, double>> _locationSubscription;
+
+  final Set<String> _places = new Set<String>();
+  final TextStyle _biggerFont = const TextStyle(
+      color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.bold);
   final TextEditingController _valueCtrl = new TextEditingController();
+  final Location _location = new Location();
+
+  NewReceiptState(){
+    _receipts = ReceiptsApp.receipts;
+    _receipts.load();
+  }
 
   @override
   void initState() {
     super.initState();
-    getPlaces();
+    _locationSubscription = _location.onLocationChanged.listen(getPlaces);
+    WidgetsBinding.instance.addObserver(this);
   }
 
-  getPlaces() async {
-    Location _location = new Location();
-    Map<String,double> location;
-    dynamic data;
+  @override
+  dispose (){
+    print('DISPASE');
+    super.dispose();
+  }
 
-    try {
-      location = await _location.getLocation;
-    } catch (e){
-      location = null;
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch(state)
+    {
+      case AppLifecycleState.paused:
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.suspending:
+        _receipts.save();
+        _locationSubscription.cancel();
+        break;
+      case AppLifecycleState.resumed:
+        _locationSubscription = _location.onLocationChanged.listen(getPlaces);
+        break;
     }
+    if(state == AppLifecycleState.paused){
+    }
+    print('STATE:$state');
+  }
 
+  getPlaces(Map<String, double> location) async {
+    dynamic data;
+//    print(location);
+    if(location['accuracy'] <= 20/2) {
+      print('remove loc subscription');
+      _locationSubscription.cancel();
+    }
     try {
       var uri = new Uri.https(
-      /*    'maps.googleapis.com', '/maps/api/place/nearbysearch/json', {
+
+/*    'maps.googleapis.com', '/maps/api/place/nearbysearch/json', {
         'key': 'AIzaSyCErmNo5wVAFa68O49BoihbeQz_Jtyk8Zk',
         'location': "-37.934747,145.038774",
         'radius': '20'
       });*/
+
       'maps.googleapis.com', '/maps/api/place/nearbysearch/json', {'key': 'AIzaSyCErmNo5wVAFa68O49BoihbeQz_Jtyk8Zk', 'location': "${location["latitude"]},${location["longitude"]}", 'radius': '20'});
 
-      var response = await http.get(uri);
-      data = JSON.decode(response.body);
+      http.Response response = await http.get(uri);
+      data = json.decode(response.body);
     } catch (e) {
-      data = e.toString();
     }
+
+//    print('DATA:${data}');
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
 
-    setState(() {
-      for (var r in data['results']) _places.add(r['name']);
-//      _places.add('test');
-    });
+    if(data != null)
+      setState(() {
+//      _places.clear();
+        for (var r in data['results']) _places.add(r['name']);
+/*
+      _places.add('test0');
+      _places.add('test1');
+      _places.add('test2');
+*/
+      });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Add Receipt'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushReceipts),
-        ],
-      ),
+      appBar: new AppBar(title: new Text('Add Receipt'), actions: <Widget>[
+        new IconButton(
+          icon: new Icon(Icons.list),
+          onPressed: () {
+            Navigator.push(context, new MaterialPageRoute(builder: (context) {
+              return new ReceiptsWidget();
+            }));
+          },
+        )
+      ]),
       body: _buildInput(),
     );
   }
 
   Widget _buildInput() {
-    _valueTextField = new TextField(controller: _valueCtrl, autofocus: true, keyboardType: TextInputType.number, textAlign: TextAlign.center, style: _biggerFont,);
-
-    return new Column(
-        children: <Widget>[
-          _valueTextField,
-          new Expanded(child: new ListView.builder(
-            itemBuilder: (context, i)
-            {
-              if(i >= _places.length)
-                return null;
-              else
-                return _buildRow(_places[i]);
-            },
-          )),
-        ]
-    );
+    return new Column(children: <Widget>[
+      new TextField(
+        controller: _valueCtrl,
+        autofocus: true,
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.left,
+        style: _biggerFont,
+        decoration: new InputDecoration(
+            labelText: '\$', contentPadding: new EdgeInsets.all(10.0)),
+      ),
+      new Expanded(child: new ListView.builder(
+        itemBuilder: (context, i) {
+          if (i >= _places.length)
+            return null;
+          else
+            return _buildPlaceRow(_places.elementAt(i));
+        },
+      )),
+    ]);
   }
 
-  Widget _buildRow(String place) {
+  Widget _buildPlaceRow(String place) {
     return new ListTile(
       title: new Text(
         place,
@@ -125,52 +164,88 @@ class ReceiptsState extends State<Receipts> {
       ),
       onTap: () {
         setState(() {
-          _receipts.add(new Reciept(new DateTime.now(), place, double.parse(_valueCtrl.text)));
+          double value = null;
+          try{
+            value = double.parse(_valueCtrl.text);
+
+          } catch (e) {}
+          if(value != null) {
+            _receipts.current.add(new Reciept(
+                new DateTime.now(), place, value));
+
+//            _receipts.save();
+            SystemNavigator.pop();
+//            exit(0);
+//            Navigator.pop(context);
+          }
+        });
+      },
+    );
+  }
+}
+
+class ReceiptsWidget extends StatefulWidget {
+  @override
+  createState() => new ReceiptsState();
+}
+
+class ReceiptsState extends State<ReceiptsWidget> {
+  Receipts _receipts;
+
+  final TextStyle _biggerFont = const TextStyle(
+      color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.bold);
+
+  ReceiptsState()
+  {
+    _receipts = ReceiptsApp.receipts;
+  }
+
+/*
+  @override
+  void initState() {
+    super.initState();
+  }
+
+*/
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Receipts'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.undo), onPressed: _undo),
+        ],
+      ),
+      body: new ListView.builder(itemBuilder: (BuildContext context, int i) {
+        if (i >= _receipts.current.length)
+          return null;
+        else
+          return _buildReceiptRow(_receipts.current[i]);
+      }),
+    );
+  }
+
+  Widget _buildReceiptRow(Reciept receipt) {
+    var formatter = new DateFormat('yyyy-MM-dd HH:mm');
+    String formatted = formatter.format(receipt.date.toLocal());
+    return new ListTile(
+      title: new Text(
+        '${formatted} ${receipt.place} ${receipt.value}',
+        style: _biggerFont,
+      ),
+      onTap: () {
+        setState(() {
+          _receipts.current.remove(receipt);
+          _receipts.checked.add(receipt);
         });
       },
     );
   }
 
-  void _pushReceipts() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _receipts.map(
-            (Reciept receipt) {
-              var formatter = new DateFormat('yyyy-MM-dd hh:mm');
-              String formatted = formatter.format(receipt.date.toLocal());
-              return new ListTile(
-                title: new Text(
-                  '${formatted} ${receipt.location} ${receipt.value}',
-                  style: _biggerFont,
-                ),
-                onTap: () {
-                  _receipts.remove(receipt);
-                  setState(() {});
-                  },
-              );
-            },
-          );
-          final divided = ListTile
-              .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
-              .toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Receipts'),
-              actions: <Widget>[
-                new IconButton(icon: new Icon(Icons.undo), onPressed: _undo),
-              ],
-
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
+  void _undo() {
+    setState((){
+      if(_receipts.checked.length > 0)
+        _receipts.current.insert(0, _receipts.checked.removeLast());
+    });
   }
-
-  void _undo(){}
 }
